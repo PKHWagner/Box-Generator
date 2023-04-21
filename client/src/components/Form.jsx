a3b8a035d90f801ee2451df4f1440abc10fe5778
PKHWagner <se.aksnow@icloud.com>
2023-04-20 20:20:01 -0800

    updated the styling

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -25,17 +25,20 @@ const Form = ({allBoxes, setAllBoxes}) => {
   }
 
   return (
-    <div className='container mt-5'>
+    <div className='container text-bg-warning bg-opacity-25 p-3 w-75 rounded-3 mt-5'>
       <form onSubmit={submitHandler}>
         <div>
-          <label htmlFor="color" className='m-3'>Choose A Color:</label>
-          <input type="text" name='color' value={box.color} onChange={handleChange}/>
+          <label htmlFor="color" className='m-4'>Choose A Color:</label>
+          <input type="text" name='color' value={box.color} className='justify-content-end' onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor="size" className='m-3'>How big of a Box?</label>
-          <input type="number" name="size" value={box.size} onChange={handleChange}/>
+          <input type="number" name="size" value={box.size} className='justify-content-end' onChange={handleChange}/>
         </div>
-        <button className='mx-auto'>Add</button>
+        <div className='d-flex justify-content-center'>
+          <button>Add</button>
+        </div>
+        
       </form>
     </div>
   )
